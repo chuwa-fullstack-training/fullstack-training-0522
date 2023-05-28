2986db0a799eb1068194181e31947fc620e898cf
axiao3 <72429720+axiao3@users.noreply.github.com>
2023-05-28 02:03:44 -0700

    Update hw3.js
    done

--- homeworks/lecture3/hw3.js
+++ homeworks/lecture3/hw3.js
@@ -1,8 +1,16 @@
 function counter() {
     // implement here
+    var result = 0;
+
+    //return a function without name
+    return function (num = 0){
+        return result += num;
+    }
+    
 }
 
 let count = counter();
 console.log(count(3));  // Output: 3
 console.log(count(5));  // Output: 8 (3 + 5)
 console.log(count());   // Output: 8
+//if there is extra () than defined, think about return a function
