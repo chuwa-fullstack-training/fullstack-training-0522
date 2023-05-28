34082622401bfd804d7fb6de73941fa7efd75e0c
axiao3 <72429720+axiao3@users.noreply.github.com>
2023-05-28 01:40:04 -0700

    Update hw2.js
    changed parameters of sum()

--- homeworks/lecture3/hw2.js
+++ homeworks/lecture3/hw2.js
@@ -2,6 +2,17 @@
  * console.log(sum(2)(3) === 5)
  * console.log(sum(2, 3) === 5)
  */
-function sum() {
+
+//comment out one another when test
+function sum(num1, num2) {
     // implement here
+    return num1 + num2;
 }
+console.log(sum(2, 3) === 5);
+
+function sum(num1){
+    return function (num2) {
+        return num1 + num2;
+    }
+}
+console.log(sum(2)(3) === 5);
