62a849e35ad335e1b9cecc7c89b266cf60c0c240
axiao3 <72429720+axiao3@users.noreply.github.com>
2023-05-29 03:44:52 -0700

    Update hw4.js
    hw4 function based before ES6 is done

--- homeworks/lecture3/hw4.js
+++ homeworks/lecture3/hw4.js
@@ -6,6 +6,10 @@ Shape.prototype.getType = function() {
     return this.type;
 }
 
+const shapeInstance = new Shape();
+console.log(shapeInstance.type);
+console.log(shapeInstance.getType());
+
 function Triangle(a, b, c) {
     this.type = 'triangle';
     this.a = a;
@@ -17,11 +21,55 @@ Triangle.prototype = Object.create(Shape.prototype);
 Triangle.prototype.constructor = Triangle;
 
 // your code goes here
+
 // 1. implement a method getPerimeter for Triangle class
 // 2. implement a method getArea for Triangle class
+
+Triangle.prototype.getPerimeter = function() {
+    return this.a + this.b + this.c;
+}
+Triangle.prototype.getArea = function() {
+    let s = (this.a + this.b + this.c) / 2;
+    return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+}
+const triangleInstance = new Triangle (1, 3, 3);
+console.log(triangleInstance.type);
+console.log(triangleInstance.getType());
+console.log(triangleInstance.getPerimeter());
+console.log(triangleInstance.getArea());
 
 // 3. implement a new class Circle. this class should inherit from Shape class, and have a radius property.
 // 4. implement a method area for Circle class
 // 5. implement a method circumference for Circle class
 
-// 6. change all code above to use ES6 class syntax
+function Circle(radius) {
+    this.type = 'circle';
+    this.radius = radius;
+}
+
+Circle.prototype = Object.create(Shape.prototype);//extends
+Circle.prototype.constructor = Circle;//constructor points to the functional object itself
+//methods
+Circle.prototype.area = function() {
+    return Math.PI * this.radius * this.radius;
+}
+Circle.prototype.circumference = function() {
+    return 2 * Math.PI * this.radius;
+}
+
+const circleInstance = new Circle(1);
+console.log(circleInstance.type);
+console.log(circleInstance.getType());
+console.log(circleInstance.area());
+console.log(circleInstance.circumference());
+
+// output:
+// shape
+// shape
+// triangle
+// triangle
+// 7
+// 1.479019945774904
+// circle
+// circle
+// 3.141592653589793
