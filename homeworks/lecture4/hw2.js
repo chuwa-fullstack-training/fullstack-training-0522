cbf816a714edd48121b3b63bdc84c1538d73c65e
Jing Wang <jingw9327@gmail.com>
2023-06-01 11:18:41 -0500

    half way through hw2.

---
+++ homeworks/lecture4/hw2.js
@@ -0,0 +1,26 @@
+// ONLY use map, filter, reduce to solve the following problems:
+
+// 1. Given an array of numbers, return an array of numbers that are doubled.
+const numbersArray1 = [1, 2, 3, 4];
+// using map
+const doubledArray1 = numbersArray1.map((num) => num * 2);
+console.log(doubledArray1);
+
+// using filter
+const doubledArray2 = numbersArray1.reduce((acc, num) => {
+  acc.push(num * 2);
+  return acc;
+}, []);
+console.log(doubledArray2);
+// 2. Given an array of numbers, return an array of numbers that are even.
+
+// 3. Reverse the string: "Hello World" -> "dlroW olleH"
+
+/**
+ * 4. Flatten the array of arrays to a single array:
+ * Example 1:
+ * Expected output: [0, 1, 2, 3, 4, 5]
+ * Example 2:
+ * const arr = [[0, 1], [2, 3], [4, [5, 6]]];
+ * Expected output: [0, 1, 2, 3, 4, 5, 6]
+ */
