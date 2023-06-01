c60e5608673b7e550451626c218acde598734eab
jessiehxw <hexiawei1999@outlook.com>
2023-05-31 21:41:18 -0500

    HW4 is completed.

--- homeworks/lecture2/hw4.js
+++ homeworks/lecture2/hw4.js
@@ -8,6 +8,7 @@ function f() {
   }
   console.log(a);
 }
+/* 7. Since var is function scoped. */
 
 // 2. When executed, what value will be output?
 function f() {
@@ -17,12 +18,16 @@ function f() {
   console.log(a);
 }
 
+/* 5. Since var is function scoped. */
+
 // 3. When executed, what value will be output?
 function f() {
   a = 3;
 }
 f();
 console.log(a);
+
+/* Answer is 3. a is automatically assigned to global scope as it is not declared. */
 
 // 4.
 var a = 5;
@@ -36,12 +41,16 @@ function second() {
 first();
 second();
 
+/* 6. Since var when outside of function scope is global scope. */
+
 // 5.
 var a = 5;
 function f() {
   var a = 7;
   console.log(a);
 }
+
+/* 7. As var a is defined with 7 inside the function, so the value of output inside function will be 7. Var is functional scoped when it's placed inside function. */
 
 // 6.
 var a = 1;
@@ -52,3 +61,5 @@ function b() {
 }
 b();
 console.log(a);
+
+/* 1. The a inside function b is declared as a function. The function declaration is hoisted to the top of the function b, and assigned a value of 10. Thus, the global scoped a is not affected by the function a inside function b. So the output value is still 1. Only when the inside variable a is not declared, it will modify the global scoped a. */
