d8e221f881cb9c09ef9f2ff73a78258e6607d372
jessiehxw <hexiawei1999@outlook.com>
2023-05-31 23:34:10 -0500

    HW5 is completed.

--- homeworks/lecture2/hw5.js
+++ homeworks/lecture2/hw5.js
@@ -4,23 +4,23 @@
 var x;
 
 if (x !== 3) {
-  console.log(y);
-  var y = 5;
+  console.log(y); // undefined, since y is not created yet
+  var y = 5; 
   if (y === 5) {
-    var x = 3;
+    var x = 3;  
   }
-  console.log(y);
+  console.log(y); // 5, since y is assigned value 5
 }
-if (x === 3) {
-  console.log(y);
+if (x === 3) {  // now x is assigned value 3
+  console.log(y); // 5 will be logged
 }
 
 
 // 2.
 var x = 3;
 if (x === 3) {
-  var x = 2;
-  console.log(x);
+  var x = 2;  
+  console.log(x); // 2, since the value is changed to 2
 }
-console.log(x);
+console.log(x);  // 2
 
