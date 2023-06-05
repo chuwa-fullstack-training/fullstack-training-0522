f82601d07e443b5b17d5d1812211e7ab2a492272
axiao3 <axiao3@asu.edu>
2023-06-05 04:41:45 -0700

    hw4 is done

--- homeworks/lecture5/hw4.js
+++ homeworks/lecture5/hw4.js
@@ -12,8 +12,12 @@ Promise.resolve(1)
   .then(res => {
     console.log(res);
   });
+  // 1 2
+  // resolve(1), when then(), pass 1 as res, console log 1 and return 2
+  // catch will not be triggered since it's resolve(1)
+  // the returned 2 will be pass to the last to then(res) as res and be printed out
 
-// // 2
+// 2
 Promise.reject(1)
   .then(res => {
     console.log(res);
@@ -26,6 +30,10 @@ Promise.reject(1)
   .then(res => {
     console.log(res);
   });
+  // 1 3
+  // reject(1), when catch() (first then will not be executed since is rejected), pass 1 as err and print out
+  // and return 3
+  // the last then will accept 3 as res and print it out
 
 //3
 function runAsync(x) {
@@ -45,3 +53,7 @@ function runReject(x) {
 Promise.all([runAsync(1), runReject(4), runAsync(3), runReject(2)])
   .then(res => console.log(res))
   .catch(err => console.log(err));
+// Error: 2
+// Promise.all(), once there is one promise rejected it will fall to catch
+// runReject(4) -> dely 4s, runReject(2) -> dely 2s
+// catch will run the first error caught, which is runReject(2)
