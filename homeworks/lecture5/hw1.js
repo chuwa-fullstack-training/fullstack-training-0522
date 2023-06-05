75b8fb3bffabbc380f6677fc4a684ab85f6aead3
axiao3 <axiao3@asu.edu>
2023-06-05 04:32:11 -0700

    hw1 is done

--- homeworks/lecture5/hw1.js
+++ homeworks/lecture5/hw1.js
@@ -4,11 +4,15 @@
 for (var i = 0; i < 5; i++) {
   setTimeout(() => console.log(i), 1000);
 }
+// 5 5 5 5 5
+// var hoisting, to the top of the file, all the value would be the same since if refers to the same var address
 
 // 2
 for (let i = 0; i < 5; i++) {
   setTimeout(() => console.log(i), 1000);
 }
+// 0 1 2 3 4
+// let in the block scope, the program can reach every iteration of the let i
 
 // 3
 for (var i = 0; i < 5; i++) {
@@ -16,6 +20,8 @@ for (var i = 0; i < 5; i++) {
     setTimeout(() => console.log(i), 1000);
   })(i);
 }
+// 0 1 2 3 4
+// this is an IIFE, which will include the var and the setTimeout can acess the value in each iteration
 
 // 4
 let fn = () => {
@@ -25,6 +31,8 @@ setTimeout(fn, 1000);
 fn = () => {
   console.log('I am another fn');
 }
+// I am fn
+// fn was invoked first when setTimeout(fn, 1000)
 
 // 5
 let obj = {
@@ -32,3 +40,5 @@ let obj = {
 }
 setTimeout(() => console.log(obj), 1000);
 obj.name = 'another obj';
+// another obj
+// the first arguments refers to the object. change the property of object is synchronized statement which will execute first
