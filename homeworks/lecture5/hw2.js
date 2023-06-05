471e9206f532388f689e9f1b2e281fd46b6f6613
axiao3 <axiao3@asu.edu>
2023-06-05 04:30:30 -0700

    hw2 is done

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -9,7 +9,24 @@
  */
 function reverseWords(str) {
   // your code here
+  let result = '';
+  let word = '';
+  let space = 0;
+  for (let i = 0; i < str.length; i++){
+    
+    if (str[i] === ' '){
+      space === 0? result = word + result : result = word + ' ' + result;
+      space++;
+      word = '';
+    }
+    else{
+      word += str[i];
+    }
+  }
+  result = word + ' ' + result;
+  
+  return result;
 }
 
 const input = 'the sky is blue'.split(''); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
-reverseWords(input);
+console.log(reverseWords(input));
