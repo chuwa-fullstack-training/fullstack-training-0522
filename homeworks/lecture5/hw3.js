8b17e1c2b07d4e0705750ed7d44d2ae4846c2c3f
axiao3 <axiao3@asu.edu>
2023-06-05 04:31:31 -0700

    hw3 is done

--- homeworks/lecture5/hw3.js
+++ homeworks/lecture5/hw3.js
@@ -9,6 +9,11 @@ new Promise((resolve, reject) => {
   console.log('e');
   reject('f');
 }).then(result => console.log(result));
+// a c e d b
+// setTimeout will be put in Web API and waiting, so a and c will be printed out first
+// then it goes to new Promise, and console log e, promise goes to pending now
+// promise.then() is a micro task which will execute the resolve status print out d
+// then setTimeout will print out b when all the tasks in the callstack are executed 
 
 // 2
 const fn = () =>
@@ -22,3 +27,7 @@ fn().then(res => {
 });
 
 console.log('start');
+// 1 start success
+// fn() invoked first and print out 1
+// the macro task script will continue runing and print out start
+// promise.then() is a micro task which will execute the resolve status and print out success
