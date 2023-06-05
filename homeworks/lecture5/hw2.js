e9a4070688073289397155b5bb316d840504f45f
Zixin Li <zli@quantumphotonics.club>
2023-06-05 14:34:53 -0400

    lecture 5 hw2 revision

--- homeworks/lecture5/hw2.js
+++ homeworks/lecture5/hw2.js
@@ -10,14 +10,16 @@
 function reverseWords(str) {
   // your code here
   str.reverse();
-  let prev=0;
+  let prev = 0;
   for (let i = 0; i < str.length; i++) {
-    if(str[i] === ' '){
-      let reversedWord=str.substr(prev,i).reverse();
-      prev=i;
+    if (i === str.length - 1 || str[i + 1] === " ") {
+      for (let j = 0; j < Math.floor((i - prev + 1) / 2); j++)
+        [str[j + prev], str[i - j]] = [str[i - j], str[j + prev]];
+      prev = i + 2;
     }
   }
 }
 
 const input = "the sky is blue".split(""); // ['t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e']
 reverseWords(input);
+console.log(input.join(""));
