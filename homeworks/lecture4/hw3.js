e6ff504afa17596d2a9baf37c2e3133872edaf2b
Zixin Li <zli@quantumphotonics.club>
2023-06-06 19:17:14 -0400

    lecture4 hw3 submission

--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -9,9 +9,33 @@
 
 // your code here
 class Singleton {
-
+  constructor() {
+    if (!Singleton.instance) {
+      Singleton.instance = this;
+    }
+    return Singleton.instance;
+  }
 }
 
 const instance1 = new Singleton();
 const instance2 = new Singleton();
-console.log(instance1, instance2, instance1 === instance2);
+console.log(instance1 === instance2);
+
+var Singleton2 = (function () {
+  var instance;
+
+  function createInstance() {
+    var object = new Object();
+    return object;
+  }
+  return {
+    getInstance: () => {
+      if (!instance) instance = createInstance();
+      return instance;
+    },
+  };
+})();
+
+var instance3 = Singleton2.getInstance();
+var instance4 = Singleton2.getInstance();
+console.log(instance3 === instance4);
