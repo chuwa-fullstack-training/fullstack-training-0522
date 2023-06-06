92f3fee6f61c1ab3eb83fcf70392b9444d81ed29
axiao3 <72429720+axiao3@users.noreply.github.com>
2023-06-06 04:13:17 -0700

    Add files via upload

---
+++ homeworks/lecture4/hw3_function.js
@@ -0,0 +1,22 @@
+/** implement Singleton pattern in both ES5 and ES6
+ * https://en.wikipedia.org/wiki/Singleton_pattern
+ * 
+ * Example:
+ * const instance1 = new Singleton();
+ * const instance2 = new Singleton();
+ * console.log(instance1 === instance2); // Output: true
+ */
+
+// your code here
+function Singleton () {  
+    if (Singleton.prototype.init) {
+        return Singleton.prototype.init;
+    }
+    Singleton.prototype.init = this;
+}
+
+
+
+const instance1 = new Singleton();
+const instance2 = new Singleton();
+console.log(instance1 === instance2); 
