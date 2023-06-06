c597434281daaa064c35f8d8ba8d98858a5bf34f
axiao3 <axiao3@asu.edu>
2023-06-06 04:08:06 -0700

    hw3 ES6 is done

--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -8,3 +8,23 @@
  */
 
 // your code here
+class Singleton {
+    
+
+    constructor(){
+        const init = this.constructor.init;
+        
+        if (init) {
+            return init;
+        }
+
+        //this.init = this;
+        this.constructor.init = this;
+    }
+    
+    
+}
+// https://stackoverflow.com/questions/1635800/javascript-best-singleton-pattern
+const instance1 = new Singleton();
+const instance2 = new Singleton();
+console.log(instance1 === instance2);
