82017f65ce4cb21f31c869c13c2c23e2ec77fdbf
axiao3 <axiao3@asu.edu>
2023-06-07 12:02:40 -0700

    hw5 is done

--- homeworks/lecture5/hw5.js
+++ homeworks/lecture5/hw5.js
@@ -2,45 +2,80 @@
 
 const https = require('https');
 
-// function httpsRequest(url) {
-//   const options = {
-//     headers: {
-//       'User-Agent': 'request'
-//     }
-//   };
-//   const request = https.get(url, options, response => {
-//     if (response.statusCode !== 200) {
-//       console.error(
-//         `Did not get an OK from the server. Code: ${response.statusCode}`
-//       );
-//       response.resume();
-//     }
+function httpsRequest(url) {
+  const options = {
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  const request = https.get(url, options, response => {
+    if (response.statusCode !== 200) {
+      console.error(
+        `Did not get an OK from the server. Code: ${response.statusCode}`
+      );
+      response.resume();
+    }
 
-//     let data = '';
-//     response.on('data', chunk => {
-//       data += chunk;
-//     });
-//     response.on('end', () => {
-//       try {
-//         // When the response body is complete, we can parse it and log it to the console
-//         console.log(JSON.parse(data));
-//       } catch (e) {
-//         // If there is an error parsing JSON, log it to the console and throw the error
-//         throw new Error(e.message);
-//       }
-//     });
-//   });
-//   request.on('error', err => {
-//     console.error(
-//       `Encountered an error trying to make a request: ${err.message}`
-//     );
-//   });
-// }
+    let data = '';
+    response.on('data', chunk => {
+      data += chunk;
+    });
+    response.on('end', () => {
+      try {
+        // When the response body is complete, we can parse it and log it to the console
+        console.log(JSON.parse(data));
+      } catch (e) {
+        // If there is an error parsing JSON, log it to the console and throw the error
+        throw new Error(e.message);
+      }
+    });
+  });
+  request.on('error', err => {
+    console.error(
+      `Encountered an error trying to make a request: ${err.message}`
+    );
+  });
+}
 
 function getJSON(url) {
   // implement your code here
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+    
+    const request = https.get(url, options, response => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Did not get an OK from the server. Code: ${response.statusCode}`));
+        response.resume();
+      }
+
+      let data = '';
+      response.on('data', chunk => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        try{
+          resolve(JSON.parse(data));
+        }
+        catch(e){
+          reject(new Error(e.message));
+        }
+      });
+    });
+
+    request.on('error', err => {
+      reject(new Error (`Encountered an error trying to make a request: ${err.message}`));
+    });
+    
+
+  });
 }
 
+//httpsRequest('https://api.github.com/search/repositories?q=javascript');
 getJSON('https://api.github.com/search/repositories?q=javascript')
   .then(response => console.log(response.items.length)) // output: 30
   .catch(err => console.log(err)); // if you remove options from https.get parameters, you might see an error
