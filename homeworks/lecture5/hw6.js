bc968aed66bafdc2b9fd1edebe9551c4e70153bf
axiao3 <axiao3@asu.edu>
2023-06-07 12:14:36 -0700

    hw6 is done

--- homeworks/lecture5/hw6.js
+++ homeworks/lecture5/hw6.js
@@ -10,10 +10,49 @@ function sequencePromise(urls) {
     return getJSON(url).then(response => results.push(response));
   }
   // implement your code here
+   const promises = urls.map(url => results.push(fetchOne(url)));
+   Promise.allSettled(results)
+          .then(allRes => console.log(allRes))
+          .catch(error => console.log(error.message));
+  
 }
 
 function getJSON(url) {
   // this is from hw5
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: {
+        'User-Agent': 'request'
+      }
+    };
+    
+    const request = https.get(url, options, response => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Did not get an OK from the server. Code: ${response.statusCode}`));
+        response.resume();
+      }
+
+      let data = '';
+      response.on('data', chunk => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        try{
+          resolve(JSON.parse(data));
+        }
+        catch(e){
+          reject(new Error(e.message));
+        }
+      });
+    });
+
+    request.on('error', err => {
+      reject(new Error (`Encountered an error trying to make a request: ${err.message}`));
+    });
+    
+
+  });
 }
 
 // function getJSON(url) {
@@ -26,3 +65,5 @@ const urls = [
   'https://api.github.com/search/repositories?q=react',
   'https://api.github.com/search/repositories?q=nodejs'
 ];
+
+sequencePromise(urls);
