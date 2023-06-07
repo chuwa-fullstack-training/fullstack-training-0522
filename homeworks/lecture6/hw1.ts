4c8f893201326f918fdf0dcc4ff8aa5b25533e51
axiao3 <axiao3@asu.edu>
2023-06-07 13:24:27 -0700

    lecture6 hw1 is done

--- homeworks/lecture6/hw1.ts
+++ homeworks/lecture6/hw1.ts
@@ -4,20 +4,31 @@ type User = {
   type: string;
 };
 
-function makeCustomer<T extends User>(u: T): T {
+function makeCustomer<T extends User>(u: T): T {//cannot guarantee that T passed is an instance extends User
   return {
     id: u.id,
     type: "customer",
-  };
+  } as T;//type assertion
 }
+let instance : User = {id: 1, type: "user"};
+console.log(makeCustomer(instance));
 
 // 2. fix the following code
 // requirement: the function should accept either two strings or two numbers at the same time,
 // so if parameters are one string and one number, it should throw an error
 function f(a: string | number, b: string | number) {
-  if (typeof a === "string") {
+  if (typeof a === "string" && typeof b === "string") {
     return `${a} : ${b}`;
-  } else {
+  } 
+  else if(typeof a === "number" && typeof b === "number"){
     return a + b;
   }
+  else{
+    throw new Error('cannot assign a and b with different types');
+  } 
+  
 }
+
+console.log(f(1,2));
+console.log(f('1','2'));
+console.log(f('1',2));
