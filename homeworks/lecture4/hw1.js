06535f0dc7269f3f007655435a7d6b621b18636f
WWang <Weiyu.Wang@us.panasonic.com>
2023-06-07 18:52:30 -0500

    modified:   homeworks/lecture4/hw1.js

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -7,4 +7,29 @@
 
 function checkValidHTML(html) {
     // implement your solution here
+    let stack = []
+    let i = 0
+    while (i < html.length) {
+        if (html[i] == '<') {
+            if (html[i + 1] == '/') {
+                if (html[i + 2] == stack[stack.length - 1]) {
+                    stack.pop()
+                    i += 3
+                } else {
+                    return false
+                }
+            } else {
+                stack.push(html[i + 1])
+                i += 2
+            }
+        } else {
+            i++
+        }
+    }
+    return stack.length == 0
 }
+
+console.log(checkValidHTML('<html><head><title>My Title</title></head></html>'))
+console.log(checkValidHTML('<html><head><title>My Title</title></head></head></html>'))
+console.log(checkValidHTML('<html><head><title>My Title</title></head></html'))
+
