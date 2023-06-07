8a85178c84733d967e7543ef41fd77cc6d373398
Yongyan Deng <deng.yo@northeastern.edu>
2023-06-07 19:52:49 -0400

    update lecture4 hw1

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -8,22 +8,14 @@
 function checkValidHTML(html) {
     // implement your solution here
     const stk = [];
+    const openTagRegex = /<(\w+)>/g;
+    const closeTageRegex = /<\/(\w+)>/g;
 
-    for(let i = 0; i < html.length; i++) {
-        if(html[i] === '<') {
-            let endTagPos = html.indexOf('>', i + 1);
-            if(endTagPos === -1) return false;
-
-            let tag = html.slice(i + 1, endTagPos);
-
-            if(tag[0] === '/') {
-                if(!stk.length) return false;
-                let openTag = stk.pop();
-                if(openTag !== tag.slice(1)) return false;
-            } 
-            else stk.push(tag);
-            i = endTagPos;
-        }
+    let match = null;
+    while(match = openTagRegex.exec(html)) stk.push(match[1]);
+    while(match = closeTageRegex.exec(html)) {
+        let openTag = stk.pop();
+        if(openTag != match[1]) return false;
     }
     return !stk.length;
 }
