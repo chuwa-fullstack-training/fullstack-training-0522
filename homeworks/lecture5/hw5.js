36cf4d58c2960a3882b1ee8f120170c78adca475
Jing Wang <jingw9327@gmail.com>
2023-06-07 15:42:42 -0500

    hw5 is updated.'

--- homeworks/lecture5/hw5.js
+++ homeworks/lecture5/hw5.js
@@ -3,7 +3,12 @@
 const https = require("https");
 
 // function httpsRequest(url) {
-//   const request = https.get(url, response => {
+//   const options = {
+//     headers: {
+//       'User-Agent': 'request'
+//     }
+//   };
+//   const request = https.get(url, options, response => {
 //     if (response.statusCode !== 200) {
 //       console.error(
 //         `Did not get an OK from the server. Code: ${response.statusCode}`
@@ -11,11 +16,11 @@ const https = require("https");
 //       response.resume();
 //     }
 
-//     let data = "";
-//     response.on("data", (chunk) => {
+//     let data = '';
+//     response.on('data', chunk => {
 //       data += chunk;
 //     });
-//     response.on("end", () => {
+//     response.on('end', () => {
 //       try {
 //         // When the response body is complete, we can parse it and log it to the console
 //         console.log(JSON.parse(data));
@@ -25,7 +30,7 @@ const https = require("https");
 //       }
 //     });
 //   });
-//   request.on("error", (err) => {
+//   request.on('error', err => {
 //     console.error(
 //       `Encountered an error trying to make a request: ${err.message}`
 //     );
@@ -34,6 +39,12 @@ const https = require("https");
 
 function getJSON(url) {
   return new Promise((resolve, reject) => {
+    const options = {
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+
     const request = https.get(url, (response) => {
       if (response.statusCode !== 200) {
         const error = new Error(
@@ -65,3 +76,7 @@ function getJSON(url) {
     });
   });
 }
+
+getJSON("https://api.github.com/search/repositories?q=javascript")
+  .then((response) => console.log(response.items.length)) // output: 30
+  .catch((err) => console.log(err)); // if you remove options from https.get parameters, you might see an error
