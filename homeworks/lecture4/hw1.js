983142a4ee0911e3472810f37a2140ec775810b3
wdw421579475 <40289283+wdw421579475@users.noreply.github.com>
2023-06-07 22:24:23 -0700

    Update lec4-hw1.js

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -15,6 +15,34 @@ function checkValidHTML(html) {
     return regex.test(html);
 }
 
+function checkValidHTML2(html) {
+    const tagPattern = /<(\w+)>/g;
+    const openingTags = [];
+    let match;
+  
+    while ((match = tagPattern.exec(html)) !== null) {
+      const tag = match[1];
+      openingTags.push(tag);
+    }
+  
+    const closingTags = html.match(/<\/(\w+)>/g) || [];
+  
+    if (openingTags.length !== closingTags.length) {
+      return false;
+    }
+  
+    for (let i = 0; i < openingTags.length; i++) {
+      const openingTag = openingTags[i];
+      const closingTag = closingTags[i].replace('/', '');
+  
+      if (openingTag !== closingTag) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+
 console.log(checkValidHTML('<html><head><title>My Title</title></head></html>'))
 console.log(checkValidHTML('<html><head><title>My Title</title></head></head></html>'))
 
