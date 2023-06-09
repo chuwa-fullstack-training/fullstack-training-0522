34aae9abdccdc6303e51efa022ad22557a1866ad
Sexy Hamilton <wanghanyu329@gmail.com>
2023-06-09 01:38:42 -0400

    finish lec 4

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -6,5 +6,12 @@
 // <html><head><title>My Title</title></head></html - true
 
 function checkValidHTML(html) {
-    // implement your solution here
+  // implement your solution here
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/xml");
+  if (doc.documentElement.querySelector("parsererror")) {
+    return doc.documentElement.querySelector("parsererror").innerText;
+  } else {
+    return true;
+  }
 }
--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -1,6 +1,6 @@
 /** implement Singleton pattern in both ES5 and ES6
  * https://en.wikipedia.org/wiki/Singleton_pattern
- * 
+ *
  * Example:
  * const instance1 = new Singleton();
  * const instance2 = new Singleton();
@@ -8,3 +8,23 @@
  */
 
 // your code here
+var Singleton = (function () {
+  var instance;
+
+  function createInstance() {
+    var object = new Object("I'm an instance");
+    return object;
+  }
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+var instance1 = Singleton.getInstance();
+var instance2 = Singleton.getInstance();
+console.log(instance1 === instance2);
