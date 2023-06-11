20e3cc4e944c31747bdc8ac06898fdbdb6de7e15
liushuaifan <89621842+liushuaifan@users.noreply.github.com>
2023-06-11 15:28:50 -0500

    Add files via upload

---
+++ homeworks/lecture7/hw2.js
@@ -0,0 +1,58 @@
+/**
+ * Implement a HTTP server that serves JSON data where user requests /api/parsetime and /api/unixtime.
+ * For example, when the user requests /api/parsetime?iso=2023-05-22T12:34:56.789Z, the server should
+ * respond with a JSON object containing only 'hour', 'minute' and 'second' properties.
+ * {
+ *  "hour": 12,
+ *  "minute": 34,
+ *  "second": 56
+ * }
+ * Similarly, when the user requests /api/unixtime?iso=2023-05-22T12:34:56.789Z, the server should
+ * respond with a JSON object containing a 'unixtime' property.
+ * {
+ *  "unixtime": 1684758896789
+ * }
+ *
+ * HINTS:
+ * 1. Use url.parse() method to parse URL strings.
+ * 2. response.writeHead(200, { contentType: 'application/json' })
+ */
+
+// your code here
+var http = require('http')
+var url = require('url')
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime: time.getTime() }
+}
+
+var server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true)
+    var time = new Date(parsedUrl.query.iso)
+    var result
+  
+    if (/^\/api\/parsetime/.test(req.url)) {
+      result = parsetime(time)
+    } else if (/^\/api\/unixtime/.test(req.url)) {
+      result = unixtime(time)
+    }
+  
+    if (result) {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(result))
+    } else {
+      res.writeHead(404)
+      res.end()
+    }
+  })
+  server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
