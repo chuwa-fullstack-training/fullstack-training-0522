760181446f81e68a0c2e1b7ccd0363d20b60ab56
jessiehxw <hexiawei1999@outlook.com>
2023-06-11 23:37:51 -0500

    HW6 is completed.

--- homeworks/lecture3/hw6.js
+++ homeworks/lecture3/hw6.js
@@ -19,11 +19,29 @@
  */
 function numIdenticalPairs(nums) {
   // implement here
+  let count = nums.reduce((acc, cur, index) => {
+    for (let i = index + 1; i < nums.length; i++) {
+      if (cur === nums[i]) acc++;
+    }
+    return acc;
+  }, 0);
+
+  return count;
+
 }
+
+// test
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3])); // Output: 4
 
 /**
  * Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
  */
 function removeVowels(s) {
   // implement here
+
+  /* This is a regular expression pattern enclosed in forward slashes (/). The pattern [aeiou] represents a character class that matches any single character that is one of the lowercase vowels 'a', 'e', 'i', 'o', or 'u'. The i flag makes the matching case-insensitive, so it matches both lowercase and uppercase vowels. The g flag ensures that all occurrences of the pattern in the string are replaced, not just the first one. */
+  return s.replace(/[aeiou]/gi, '');
 }
+
+// test
+console.log(removeVowels('leetcodeisacommunityforcoders')); // Output: 'ltcdscmmntyfrcdrs'
