b812e291098d2a93f7656f63ec0219f61fdd7d23
jessiehxw <hexiawei1999@outlook.com>
2023-06-11 20:39:04 -0500

    HW2 completed.

--- homeworks/lecture3/hw2.js
+++ homeworks/lecture3/hw2.js
@@ -2,6 +2,21 @@
  * console.log(sum(2)(3) === 5)
  * console.log(sum(2, 3) === 5)
  */
-function sum() {
+function sum(...nums) {
     // implement here
+    if (nums.length === 2) {
+        return nums[0] + nums[1];
+    }
+
+    // if there is only one number, return a function that takes another number
+    if (nums.length === 1) {
+        return (num) => nums[0] + num;
+    }
+
+    // if there is no number, return 0
+    return 0;
 }
+
+// test
+console.log(sum(2)(10))
+console.log(sum(2, 3))
