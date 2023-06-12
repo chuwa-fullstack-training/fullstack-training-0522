197c311d3f812f34e6f17b1f9b20d3491da23ab5
jessiehxw <hexiawei1999@outlook.com>
2023-06-12 01:06:38 -0500

    HW1 completed.

--- homeworks/lecture4/hw1.js
+++ homeworks/lecture4/hw1.js
@@ -7,4 +7,41 @@
 
 function checkValidHTML(html) {
     // implement your solution here
+
+    // use stack to store opening tags
+    const stack = [];
+    // use regex to find all tags
+    const tags = html.match(/<[^>]+>?/g);
+    
+
+    for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i];
+        // get tag name from tag using regex
+        const tagName = tag.match(/<([^>]+)>?/)[1];
+
+
+        if (tagName[0] === '/') {
+            // closing tag
+
+            // if there is no opening tag, return false as there shouldn't be closing tag
+            if (stack.length === 0) return false;
+            // if the closing tag doesn't match the last opening tag, return false
+            const lastTag = stack.pop();
+            if (lastTag !== tagName.slice(1)) return false;
+        } else {
+            // add opening tag to stack
+            stack.push(tagName);
+        }
+    }
+    
+    // if there is still opening tag left in stack, return false
+    if (stack.length > 0) return false;
+
+    return true;
 }
+
+// test
+console.log(checkValidHTML('<html><head><title>My Title</title></head></html>'));
+console.log(checkValidHTML('<html><head><title>My Title</title></head></head></html>'));
+console.log(checkValidHTML('<html><head><title>My Title</title></head></html'));
+console.log(checkValidHTML('<html><head><title><love>My Title</title></head></html>'));
