05fec0309777e410ba3aad465f6a91c5cf1228a6
jessiehxw <hexiawei1999@outlook.com>
2023-06-11 23:08:49 -0500

    HW5 is completed.

--- homeworks/lecture3/hw5.js
+++ homeworks/lecture3/hw5.js
@@ -13,4 +13,26 @@
  */
 function User() {
     // implement here
+    let password;
+
+    this.setPassword = (pw) => {
+        if (password) throw new Error('Password already set');
+        password = pw;
+    }
+
+    this.checkPassword = (pw) => {
+        return password === pw;
+    }
+
+    this.password = undefined;
 }
+
+// test
+const user = new User();
+user.setPassword('123456'); 
+console.log(user.checkPassword('123456')); // true
+console.log(user.checkPassword('123')); // false
+console.log(user.password); // undefined
+user.setPassword('123');
+user.checkPassword('123');
+user.password;
