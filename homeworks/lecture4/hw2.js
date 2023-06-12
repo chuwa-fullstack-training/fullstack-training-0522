b72fcc366771655dcf46aab8edcc19a235148cae
jessiehxw <hexiawei1999@outlook.com>
2023-06-12 18:45:05 -0500

    HW2 completed.

--- homeworks/lecture4/hw2.js
+++ homeworks/lecture4/hw2.js
@@ -1,10 +1,17 @@
 // ONLY use map, filter, reduce to solve the following problems:
 
 // 1. Given an array of numbers, return an array of numbers that are doubled.
+const arr = [1, 2, 3, 4, 5];
+const doubled = arr.map((num) => num * 2);
 
 // 2. Given an array of numbers, return an array of numbers that are even.
+const even = arr.filter((num) => num % 2 === 0);
 
 // 3. Reverse the string: "Hello World" -> "dlroW olleH"
+const str = 'Hello World';
+const strArr = str.split('');
+const reversed = strArr.reduce((acc, cur) => {return cur + acc}, '');
+console.log(reversed);
 
 /**
  * 4. Flatten the array of arrays to a single array:
@@ -15,3 +22,20 @@
  * const arr = [[0, 1], [2, 3], [4, [5, 6]]];
  * Expected output: [0, 1, 2, 3, 4, 5, 6]
  */
+const arr2 = [[0, 1], [2, 3], [4, [5, 6]]];
+const result = [];
+
+function flatten(arr) {
+
+    return arr.reduce((acc, cur) => {
+        if (Array.isArray(cur)) {
+            return acc.concat(flatten(cur));
+        }
+        else {
+            return acc.concat(cur);
+        }
+    }, []);
+    
+}
+const flattened = flatten(arr2);
+console.log(flattened);
