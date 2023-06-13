6ceba5334d6e7f8525594e5ac06f19f106f90e8f
jessiehxw <hexiawei1999@outlook.com>
2023-06-13 08:04:48 -0500

    HW3 completed.

--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -8,3 +8,35 @@
  */
 
 // your code here
+// ES5
+var Singleton = (function () {
+    // store the instance of the singleton object
+    var instance;
+
+    // constructor
+    function Singleton() {
+        // if the instance already exists, return it
+        if (instance) {
+            return instance;
+        }
+        // otherwise, create a new instance
+        instance = this;
+    }
+
+    return Singleton;
+})();
+
+// ES6
+class Singleton {
+    constructor() {
+        if (!Singleton.instance) {
+            Singleton.instance = this;
+        }
+        return Singleton.instance;
+    }
+}
+
+// Test
+const instance1 = new Singleton();
+const instance2 = new Singleton();
+console.log(instance1 === instance2); // Output: true
