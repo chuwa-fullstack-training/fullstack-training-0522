78a17064f1cfe4da463f2f07e0bb1f6427ad8069
axiao3 <axiao3@asu.edu>
2023-06-14 03:20:28 -0700

    L7 hw1 is done

--- homeworks/lecture7/hw1.js
+++ homeworks/lecture7/hw1.js
@@ -7,9 +7,39 @@
  * 1. Use fs.readdir() method to read the contents of a directory.
  * 2. Use path.extname() method to get the extension of a file. (optional)
  * 3. Use process.argv to get command-line arguments.
- *  - process.argv[0] is the path to the node program
- *  - process.argv[1] is the path to the script file
- *  - process.argv[2] is the first command-line argument
+ *  - process.argv[0] is the path to the node program - C:\Program Files\nodejs\node.exe
+ *  - process.argv[1] is the path to the script file - fullstack-training-0522\homeworks\lecture7\hw1.js
+ *  - process.argv[2] is the first command-line argument - undefined -> target dirpath
  */
 
 // your code here
+
+const res = require('express/lib/response');
+const fs = require('fs');
+const path = require('path');
+
+console.log(process.argv[2]);
+let result = [];
+let dirpath = process.argv[2];
+let ext = ".txt";
+
+fs.readdir(dirpath, "utf8", (error, files) => {
+    if (!files || !files.length){
+        console.log("no such files");
+    }
+    if(error){
+        console.log(error);
+    }
+    else{
+        for(let i = 0; i < files.length; i++){
+            let filename = files[i];
+            if(path.extname(filename) == ext){
+                result.push(filename);
+            }
+            
+        }
+    }
+    console.log(result);
+})
+
+
