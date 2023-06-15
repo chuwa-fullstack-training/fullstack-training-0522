6246481187a198c231dc55b272b8abaf151e86b1
axiao3 <axiao3@asu.edu>
2023-06-15 04:02:55 -0700

    L7 hw2 is done

--- homeworks/lecture7/hw2.js
+++ homeworks/lecture7/hw2.js
@@ -19,3 +19,43 @@
  */
 
 // your code here
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url);
+    let method = parsedUrl.pathname;
+    let timestr = parsedUrl.query;
+    let searchParams = new URLSearchParams(timestr);
+    let isostr = searchParams.get("iso");
+    let result;
+    
+    if (method === '/api/parsetime') {
+      let time = isostr.substring(11, 19).split(':', 3);
+
+      result = {
+        hour: parseInt(time[0]),
+        minute: parseInt(time[1]),
+        second: parseInt(time[2])
+      };
+      //console.log(JSON.stringify(result));
+    }
+    else if (method === '/api/unixtime'){
+      let date = new Date (isostr);
+      result = {
+        unixtime: date.getTime()
+      }
+      //console.log(date.getTime()); 
+      
+    }
+
+    res.writeHead(200, { contentType: 'application/json' });
+    res.end(JSON.stringify(result));
+
+    
+  });
+  
+  server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+  
