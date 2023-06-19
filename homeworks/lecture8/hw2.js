11e9bd094cd38c36446d41dcfae61fbe280df4fe
wdw421579475 <40289283+wdw421579475@users.noreply.github.com>
2023-06-18 18:37:03 -0700

    upload lec8-hw2

---
+++ homeworks/lecture8/hw2.js
@@ -0,0 +1,73 @@
+/**
+ * https://hn.algolia.com/api
+ *
+ * write a router function that takes two query parameters: query1 and query2
+ * and returns the partial result from the following query in order:
+ * https://hn.algolia.com/api/v1/search?query=query1&tags=story
+ * https://hn.algolia.com/api/v1/search?query=query2&tags=story
+ *
+ * e.g. http://localhost:3000/hw2?query1=apple&query2=banana
+ *
+ * result from https://hn.algolia.com/api/v1/search?query=apple&tags=story:
+ * {
+ *  "hits": [
+ *   {
+ *   "created_at": "2020-11-12T21:00:12.000Z",
+ *   "title": "macOS unable to open any non-Apple application",
+ *   ...
+ *   }
+ * ]}
+ * 
+ * result from https://hn.algolia.com/api/v1/search?query=banana&tags=story:
+ * {
+ *  "hits": [
+ *   {
+ *   "created_at": "2010-06-14T12:54:07.000Z",
+ *   "title": "Banana equivalent dose",
+ *   ...
+ *   }
+ * ]}
+ * 
+ * final result from http://localhost:3000/hw2?query1=apple&query2=banana:
+ * {
+ *   "apple":
+ *   {
+ *     "created_at": "2020-11-12T21:00:12.000Z",
+ *     "title": "macOS unable to open any non-Apple application"
+ *   },
+ *  "banana":
+ *  {
+ *   "created_at": "2010-06-14T12:54:07.000Z",
+ *   "title": "Banana equivalent dose"
+ *  }
+ * }
+ */
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/hw2', async (req, res) => {
+  const query1 = req.query.query1;
+  const query2 = req.query.query2;
+
+  try {
+    const response1 = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query1}&tags=story`);
+    const response2 = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query2}&tags=story`);
+
+    const result = {
+      [query1]: response1.data.hits[0],
+      [query2]: response2.data.hits[0]
+    };
+
+    res.json(result);
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
