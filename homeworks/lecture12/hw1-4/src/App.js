da6a4607d89cdb5b3e6cd2aa31e94e7d862caa2c
Zixin Li <zli@quantumphotonics.club>
2023-06-20 00:45:27 -0400

    lecture12 hw1-4 submission

---
+++ homeworks/lecture12/hw1-4/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Hw1 from "./components/hw1";
+import Hw2 from "./components/hw2";
+import Hw3 from "./components/hw3";
+import Hw4 from "./components/hw4";
+
+function App() {
+  return (
+    <div className="App">
+      <Hw1 />
+      <Hw2 />
+      <Hw3 />
+      <Hw4 />
+    </div>
+  );
+}
+
+export default App;
---
+++ homeworks/lecture12/hw1-4/src/components/hw2.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Hw2 extends React.Component {
+  render() {
+    return (
+      <div className="hw2">
+        <h2 style={{ border: "none", borderTop: "dotted red 3px" }}>
+          Homework 2
+        </h2>
+        <header>Header</header>
+        <nav>Nav</nav>
+        <div className="main-content" style={{ border: "none" }}>
+          <aside> Aside</aside>
+          <section> Section </section>
+        </div>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
+}
+
+export default Hw2;
