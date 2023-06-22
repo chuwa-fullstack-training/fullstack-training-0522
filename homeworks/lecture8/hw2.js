1ecea455b462bf1a39b1673ff468dfc2c333d7aa
axiao3 <axiao3@asu.edu>
2023-06-22 04:04:40 -0700

    L8 hw2,3 is done

--- homeworks/lecture8/hw2.js
+++ homeworks/lecture8/hw2.js
@@ -42,3 +42,51 @@
  *  }
  * }
  */
+
+//v1/search?
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const axios = require('axios');
+const url = require('url');
+
+
+const app = express();
+const router = express.Router();
+
+router.get('/hw2', async (req, res) => {
+    const query = req.query;
+    const query1 = req.query?.query1;
+    const query2 = req.query?.query2;
+    let result = {};
+
+    let hnUrl1 = `https://hn.algolia.com/api/v1/search?query=${query1}&tags=story`;
+    let hnUrl2 = 'https://hn.algolia.com/api/v1/search?' + `query=${query2}&tags=story`;
+
+    let res1 = await axios.get(hnUrl1);
+    let res2 = await axios.get(hnUrl2);
+    //console.log(res1.data.hits);
+    let res1Props = res1.data?.hits[0];
+    let res2Props = res2.data?.hits[0];
+
+    const res1name = `${query1}`;
+    const res2name = `${query2}`;
+    result = {
+        [res1name] : {
+            'created_at' : res1Props.created_at,
+            'title' : res1Props.title
+        },
+        [res2name] : {
+            'created_at': res2Props.created_at,
+            'title': res2Props.title
+        }
+    }
+
+    console.log(result);
+})
+
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
--- homeworks/lecture8/hw3.js
+++ homeworks/lecture8/hw3.js
@@ -1,3 +1,42 @@
 /**
  * Refactor hw3 in lecture 7 to use Express.js with template engine.
  */
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('this is the home page');
+})
+
+app.get('/about', (req, res) => {
+    res.send('this is the about page');
+})
+
+app.get('/home.html', (req, res) => {
+    // console.log(req.query);
+    const titleshow = req.query?.title;
+    const contentshow = req.query?.content;
+    res.render('index', {'title' : titleshow, 'content' : contentshow});
+})
+
+app.post('/create-post', (req, res) => {
+   const title = req.body?.title;
+   const content = req.body?.content;
+//    console.log(req.body);
+//    console.log(`?title=${title}&content=${content}`);
+   res.redirect('/home.html' + `?title=${title}&content=${content}`);
+    
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
