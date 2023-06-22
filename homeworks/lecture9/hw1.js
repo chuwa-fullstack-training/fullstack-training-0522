0e0d611f352afe6c614b945159f45eb82f2e9b7a
wdw421579475 <40289283+wdw421579475@users.noreply.github.com>
2023-06-21 23:53:46 -0700

    Update hw1.js

--- homeworks/lecture9/hw1.js
+++ homeworks/lecture9/hw1.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://training:shangan@fullstack-training.gw3nkbl.mongodb.net/<database>', 
-  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error('Failed to connect to MongoDB:', error));
+mongoose.connect('mongodb+srv://training:shangan@fullstack-training.gw3nkbl.mongodb.net/<database>', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+  })
+  .then(() => {
+        console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+        console.log('Error connecting to MongoDB', err);
+  });
 
 // Define schemas
 const EmployeeSchema = new mongoose.Schema({
