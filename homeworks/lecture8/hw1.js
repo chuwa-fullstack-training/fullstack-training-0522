3f9a6970d9985e0fe5e79f7b9c9ab02f32616680
Sexy Hamilton <wanghanyu329@gmail.com>
2023-06-22 05:53:56 -0400

    update lec 8

--- homeworks/lecture8/hw1.js
+++ homeworks/lecture8/hw1.js
@@ -9,3 +9,55 @@
  *    You don't need to handle the case like http://localhost:3000/hw1/test/test/txt.
  * 3. hw2 should be able to handle requests with query strings like it did in lecture 7;
  */
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const r1 = express.Router();
+const r2 = express.Router();
+
+//hw1
+r1.get("/hw1/:dir/:txt", (req, res) => {
+  const dir = req.params?.dir;
+  const ext = req.params?.ext;
+  console.log(dir, ext``);
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      res.status(500).json("Internal Server Error");
+      return;
+    } else {
+      const filteredFiles = files.filter(
+        (file) => path.extname(file) === `${ext}`
+      );
+      res.send(JSON.stringify(filteredFiles));
+    }
+  });
+});
+
+//hw2
+r2.get("/api/parsetime", (req, res) => {
+  const iso = req.query.iso;
+  const date = new Date(iso);
+  const result = {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds(),
+  };
+  res.send(JSON.stringify(result));
+});
+
+r2.get("/api/unixtime", (req, res) => {
+  const iso = req.query.iso;
+  const date = new Date(iso);
+  const result = {
+    unixtime: date.getTime(),
+  };
+  res.send(JSON.stringify(result));
+});
+
+app.use("/hw1", r1);
+app.use("/hw2", r2);
+app.listen(3000, () => {
+  console.log("Server listen on port 3000!");
+});
--- homeworks/lecture8/hw2.js
+++ homeworks/lecture8/hw2.js
@@ -17,7 +17,7 @@
  *   ...
  *   }
  * ]}
- * 
+ *
  * result from https://hn.algolia.com/api/v1/search?query=banana&tags=story:
  * {
  *  "hits": [
@@ -27,7 +27,7 @@
  *   ...
  *   }
  * ]}
- * 
+ *
  * final result from http://localhost:3000/hw2?query1=apple&query2=banana:
  * {
  *   "apple":
@@ -42,3 +42,32 @@
  *  }
  * }
  */
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const router = express.Router();
+
+router.get("/hw2", async (req, res) => {
+  const query1 = req.query.query1;
+  const query2 = req.query.query2;
+  try {
+    const res1 = await axios.get(
+      `https://hn.algolia.com/api/v1/search?query=${query1}&tags=story`
+    );
+    const res2 = await axios.get(
+      `https://hn.algolia.com/api/v1/search?query=${query2}&tags=story`
+    );
+    const result = {
+      [query1]: response1.data.hits[0],
+      [query2]: response2.data.hits[0],
+    };
+    res.json(result);
+  } catch (e) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.use("/", router);
+app.listen(3000, () => {
+  console.log("Server listen on port 3000");
+});
