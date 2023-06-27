401419118bc1f4a400088730c05fb3a6fa8ff30e
Yongyan Deng <deng.yo@northeastern.edu>
2023-06-26 23:29:39 -0400

    modify homeworks/lecture14/hw/src/index.js

--- homeworks/lecture14/hw/src/index.js
+++ homeworks/lecture14/hw/src/index.js
@@ -11,7 +11,7 @@ import Hw3 from './Hw3';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hw2 />
+    <Hw3 />
   </React.StrictMode>
 );
 
